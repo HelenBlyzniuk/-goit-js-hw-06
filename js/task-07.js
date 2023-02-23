98706fa0116f2c0adc7134c28ad7c1fe1cbd0e06
HelenBlyzniuk <helenblyzniuk2@gmail.com>
2023-02-23 23:13:50 +0300

    task 7 is completed

--- js/task-07.js
+++ js/task-07.js
@@ -6,18 +6,9 @@ const spanEl = document.querySelector('#text');
 inputEl.addEventListener('input', changeFontSize);
 
 
-let fontMinSize = parseInt(inputEl.getAttribute('min'));
-
-
-
-
 function changeFontSize(event) {
     
-    fontMinSize += 1;
-    console.log(fontMinSize);
-    spanEl.style.fontSize = `${fontMinSize} px`;
-    console.log(spanEl);
-    // console.log(event.currentTarget.style.fontSize = '24px');
-    
+
+    spanEl.style.fontSize = event.currentTarget.value + 'px';
 
 }
